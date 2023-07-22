3563671a2165c715648fa45dae651a1470bc1506
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2023-07-22 16:35:23 +0900

    build: update

--- Publish.sh
+++ Publish.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./Publish.sh <major/minor/patch>"
+    exit 1
+fi
+
+git switch main
+git pull
+git merge develop
 npm version $1
+git commit
 git push
 git push --tags
